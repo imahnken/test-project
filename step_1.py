1b7aebd5fa5aad7f1d02edce271bfa11cdc37fee
Igor <igor.mahnken@yahoo.com>
2020-02-27 02:13:44 -0500

    небольшое редактирование в мастер ветке

--- step_1.py
+++ step_1.py
@@ -4,13 +4,12 @@ print('whatis your name?;')
 # What is Lorem Ipsum?;
 # Lorem Ipsum is simply dummy text of the printing and;
 # typesetting industry. Lorem Ipsum has been the industry's;
-# standard dummy text ever since the 1500s,;
-# when an unknown printer took a galley of type and;
-# scrambled it to make a type specimen book.;
-#
+
 a=0
 while a<10:
     print('ippconfig')
     a+=2
 # ok Заказчик ушел
 # продолжаем работу,
+
+# еще изменение на мастер ветке
